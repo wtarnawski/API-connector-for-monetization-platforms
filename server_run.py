e2b6d71ba6a1c42284f5962d9e4f8ab10963dff5
wtarnawski <w.tarnawski@tutanota.com>
2020-09-23 10:21:29 +0200

    server deployment

--- server_run.py
+++ server_run.py
@@ -1,7 +1,14 @@
-from server.app import app
+from flask import Flask
+
+app = Flask(__name__)
+
+
 @app.route('/')
 @app.route('/index')
 def index():
     from merge_data_sources_and_push_to_spreadsheets import run
     run()
     return "OK"
+
+
+app.run()
